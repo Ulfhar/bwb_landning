f46488b328f1d6d1192dc4446cde7052bdf23eac
Ulfhar <kagitin.ulv@gmail.com>
2023-10-12 19:18:51 +0300

    added gallery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
+
+function expandImage(element) {
+    const expandedBG = document.createElement('div');
+    expandedBG.className = 'expanded-bg';
+    expandedBG.onclick = closeImage;
+
+    const expandedImg = document.createElement('img');
+    expandedImg.src = element.src;
+    expandedImg.className = 'expanded-image';
+
+    const closeIcon = document.createElement('img');
+    closeIcon.src = 'images/x.png';
+    closeIcon.className = 'close-icon';
+    closeIcon.onclick = closeImage;
+
+    expandedBG.appendChild(expandedImg);
+    expandedBG.appendChild(closeIcon);
+    document.body.appendChild(expandedBG);
+
+    expandedBG.style.display = 'block';
+}
+
+function closeImage() {
+    const expandedBG = document.querySelector('.expanded-bg');
+    if (expandedBG) {
+        document.body.removeChild(expandedBG);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const menuIcon = document.getElementById('menu-icon');
     const dropdownMenu = document.querySelector('.dropdown-menu');
