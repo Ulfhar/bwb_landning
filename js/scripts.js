b80d8c651579b7ea9e6eae15c2b530a42f416312
Ulfhar <kagitin.ulv@gmail.com>
2024-11-06 14:29:26 +0200

    optimized gallery and changed font color

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,45 @@
-
 function expandImage(element) {
-    const expandedBG = document.createElement('div');
-    expandedBG.className = 'expanded-bg';
-    expandedBG.onclick = closeImage;
+    let expandedBG = document.querySelector('.expanded-bg');
+    if (!expandedBG) {
+        expandedBG = document.createElement('div');
+        expandedBG.className = 'expanded-bg';
+        expandedBG.onclick = closeImage;
+        document.body.appendChild(expandedBG);
+    }
+    expandedBG.innerHTML = '';
+
     const expandedImg = document.createElement('img');
     expandedImg.src = element.src;
     expandedImg.className = 'expanded-image';
+
     const closeIcon = document.createElement('img');
     closeIcon.src = 'images/x.png';
     closeIcon.className = 'close-icon';
     closeIcon.onclick = closeImage;
+
     expandedBG.appendChild(expandedImg);
     expandedBG.appendChild(closeIcon);
-    document.body.appendChild(expandedBG);
     expandedBG.style.display = 'block';
 }
+
 function closeImage() {
     const expandedBG = document.querySelector('.expanded-bg');
     if (expandedBG) {
         document.body.removeChild(expandedBG);
     }
 }
-document.addEventListener('DOMContentLoaded', function() {
+
+document.addEventListener('DOMContentLoaded', function () {
     const menuIcon = document.getElementById('menu-icon');
     const dropdownMenu = document.querySelector('.dropdown-menu');
-    menuIcon.addEventListener('click', function() {
-        toggleDropdown();
+
+    menuIcon.addEventListener('click', () => {
+        dropdownMenu.classList.toggle('show');
     });
-    document.addEventListener('click', function(event) {
+
+    document.addEventListener('click', function (event) {
         if (!menuIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
-            dropdownMenu.style.display = 'none';
+            dropdownMenu.classList.remove('show');
         }
     });
-    function toggleDropdown() {
-        if (dropdownMenu.style.display === 'none' || dropdownMenu.style.display === '') {
-            dropdownMenu.style.display = 'block';
-        } else {
-            dropdownMenu.style.display = 'none';
-        }
-    }
 });
